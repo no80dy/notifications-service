42814e2b2962f412ee6446baa460410fd94d917d
no80dy <no8odyas@gmail.com>
2024-01-26 17:37:50 +0300

    Добавлен nginx в сервис, также compose файл разделен на состовляющие для более удобного использования

--- notifications/src/entrypoint-fastapi.sh
+++ notifications/src/entrypoint-fastapi.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
 KAFKA_BROKERS=("kafka-0:9092" "kafka-1:9092" "kafka-2:9092")
-RABBITMQ_BROKERS=("stats:5672")
+RABBITMQ_BROKERS=("stats:5672" "queue-disc1:5672" "queue-ram1:5672")
 
 MAX_ATTEMPTS=30
-SLEEP_INTERVAL=5
 
 exponential_backoff() {
     local broker="$1"
     local attempt=0
+    SLEEP_INTERVAL=1
 
     while [ $attempt -lt $MAX_ATTEMPTS ]; do
         nc -zv $broker 2>/dev/null
 
         if [ $? -eq 0 ]; then
-            echo "Broker $broker is ready!"
+            echo "Service $broker is ready!"
             return 0
         else
-            echo "Attempt $((attempt + 1)): Broker $broker not yet reachable. Retrying in $SLEEP_INTERVAL seconds..."
+            echo "Attempt $((attempt + 1)): Service $broker not yet reachable. Retrying in $SLEEP_INTERVAL seconds..."
             sleep $SLEEP_INTERVAL
             ((attempt++))
+            SLEEP_INTERVAL=$((2**attempt))
         fi
     done
 
-    echo "Maximum attempts reached for broker $broker. Kafka cluster is still not fully reachable. Exiting..."
+    echo "Maximum attempts reached for service $broker. Service is still not fully reachable. Exiting..."
     return 1
 }
 
@@ -34,7 +35,7 @@ check_kafka_cluster() {
         exponential_backoff $broker || return 1
     done
 
-    echo "All Kafka brokers are ready. Starting UGC service..."
+    echo "All Kafka brokers are ready!"
     return 0
 }
 
@@ -45,10 +46,11 @@ check_rabbitmq_cluster() {
     exponential_backoff $node || return 1
   done
 
-  echo "All RabbitMQ are ready. Starting notification API service..."
+  echo "All RabbitMQ are ready!"
   return 0
 }
 
 check_kafka_cluster
 check_rabbitmq_cluster
+
 gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
